62d9992fd57687cd03d3bca319ee89c3a4451c4d
enzo-rs <enzorojoall@gmail.com>
2021-05-05 19:58:24 +0200

    Fixed home route

--- src/pages/partials/Header/Nav.jsx
+++ src/pages/partials/Header/Nav.jsx
@@ -9,7 +9,7 @@ function Nav(props) {
 
   return (
     <ul>
-      <li><Link to='/home'>LOGO</Link></li>
+      <li><Link to='/'>LOGO</Link></li>
       <li><Link to='/createPlace'>LOUER</Link></li>
       <li><Link to='/bookings'>BOOKING</Link></li>
       <li><Link to='/consult'>consult</Link></li>
