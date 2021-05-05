31ca934a3e53a302823d16970a49e50f735ca47d
enzo-rs <enzorojoall@gmail.com>
2021-05-05 16:19:50 +0200

    Add Header-searchBar and nav JSX

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,7 +2,8 @@ import './button.scss';
 
 function Button(props) {
   return (
-        <button className={props.size} onClick={props.handleClick}>{props.value}</button>
+        <button className={`${props.size} ${props.color}`}
+        onClick={props.handleClick}>{props.value}</button>
   );
 }
 
